3aea05a760e413b2168434800a410dd45ae75b51
Diego Espinosa <diegoeespinosag18@gmail.com>
2024-12-05 11:36:08 -0500

    tabla de secuencia

---
+++ ManejoCicloFor/src/paquete04/ejercicio011.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete04;
+
+/**
+ *
+ * @author utpl
+ */
+public class ejercicio011 {
+
+    public static void main(String[] args) {
+       
+        String mensajeFinal = "";
+        int i, num1,num2,num3;
+        
+        for (i = 30; i >=10; i--) {
+        
+            System.out.printf(i + "-"+ i*2 +"-"+ i*3 +"-"+i*4+"\n");
+
+        }
+
+    }
+}
